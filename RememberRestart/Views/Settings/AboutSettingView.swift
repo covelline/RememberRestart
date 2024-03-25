74484a5e8bb4a31c6a98792c7a70660344db8957
mironal <m.yamaguchi@covelline.com>
2024-03-25 18:38:59 +0900

    add icon & repo

--- RememberRestart/Views/Settings/AboutSettingView.swift
+++ RememberRestart/Views/Settings/AboutSettingView.swift
@@ -8,16 +8,20 @@ struct AboutSettingView: View {
     var body: some View {
         Form {
             HStack {
+                Image(.app)
+                    .resizable()
+                    .frame(width: 128, height: 128)
                 VStack(alignment: .leading) {
                     Text(verbatim: "Remember Restart")
                         .font(.title)
                     Text("Version: \(appVersion)")
                         .foregroundColor(.secondary)
+
+                    Link(String("covelline, LLC"), destination: URL(string: "https://covelline.com/")!)
+                        .foregroundColor(.accentColor)
+                    Link(String("GitHub Repository"), destination: URL(string: "https://github.com/covelline/RememberRestart")!)
+                        .foregroundColor(.accentColor)
                 }
-            }
-            HStack {
-                Link(String("covelline, LLC"), destination: URL(string: "https://covelline.com/")!)
-                    .foregroundColor(.accentColor)
             }
         }
     }
